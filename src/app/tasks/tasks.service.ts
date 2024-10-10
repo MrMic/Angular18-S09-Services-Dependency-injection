9458876c18f6a5c473a4ff63df3949aeb56b562d
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-10-10 09:48:42 +0200

    feat(UI/UX): Outsource & Reuse Logic w/ Services

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, signal } from "@angular/core";
-import { Task } from "./task.model";
+
+import { Task, TaskStatus } from "./task.model";
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,11 @@ export class TasksService {
       status: 'OPEN'
     }
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) => oldTasks.map((task) => {
+      return task.id === taskId ? { ...task, status: newStatus } : task
+    }));
   }
 }
